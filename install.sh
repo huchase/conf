45d6df372fba6ef53ab2939ad620337f1164e271
huchase <ch3203078092@gmail.com>
2022-10-13 21:31:52 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
-#!/usr/bin/zsh
+#!/usr/bin/sh
 
+### first
 sudo apt update
 sudo apt upgrade
 
-### base_tool
-sudo apt install -y git wget curl vim tmux gcc make zsh neofetch
+### base
+sudo apt install -y git wget curl gcc make
+
+### sh
+sudo apt install -y tmux zsh neofetch
+
+### gnome
+sudo apt install -y gnome-tweaks
+
+### editor
+sudo apt install -y vim neovim
+
+### multi-media
+sudo apt install -y obs-studio vlc
+
+### perf
+sudo apt install stress sysstat
+
+### dev-env
+sudo apt install python3 python3-pip
